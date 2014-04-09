cad1ab13d04b84b04834dde2358b4b73f796a884
Javier Bianciotto <jbianciotto@liveops.com>
2014-04-09 18:13:40 -0300

    Added missing files

---
+++ MovieSuggest/Schema/Result/MovieGenre.pm
@@ -0,0 +1,13 @@
+package MovieSuggest::Schema::Result::MovieGenre;
+
+
+use base qw/DBIx::Class::Core/;
+
+__PACKAGE__->table('movie_genre');
+__PACKAGE__->add_columns( qw/movie_id genre_id/);
+__PACKAGE__->set_primary_key( qw/movie_id genre_id/);
+
+__PACKAGE__->belongs_to(movie, "MovieSuggest::Schema::Result::User", "movie_id");
+__PACKAGE__->belongs_to(genre, "MovieSuggest::Schema::Result::Genres", "genre_id");
+
+1;
